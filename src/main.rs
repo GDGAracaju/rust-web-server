94adb0acbde8fd954be29484dae1efd0552f554a
Cassio-Tieres <tieresbomfim@gmail.com>
2024-11-23 20:44:25 -0300

    Apply Hyper dependencies & add necessary imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
 fn main() {
     println!("I'm a Rustacean!")
 }
