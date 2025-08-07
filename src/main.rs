c5c009c05c6aa8559c45024fc348100e06dbedd2
Kelvin Modesto <km.modesto@gmail.com>
2025-08-07 20:10:12 -0300

    refactor: add axum, reqwest, serde, serde_json and dotenvy

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,11 @@
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{Request, Response};
-use hyper_util::rt::TokioIo;
-use std::convert::Infallible;
-use std::net::SocketAddr;
-use tokio::net::TcpListener;
+use axum::{http::StatusCode, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello World"))))
-}
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let add = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let listener = TcpListener::bind(add).await?;
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello World" }));
 
-    loop {
-        let (stream, _) = listener.accept().await?;
-        let io = TokioIo::new(stream);
-        tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new()
-                .serve_connection(io, service_fn(hello))
-                .await
-            {
-                eprintln!("Error serving connection: {:?}", err);
-            }
-        });
-    }
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3456").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
